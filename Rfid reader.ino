a481567e0587cb930f696432e24faba24871d7b0
Johannes Gehrig <udjpk@student.kit.edu>
2018-01-25 21:03:15 +0100

    updated arduino code
    implemented multiple products and first support of a thermoprinter as output

--- Rfid reader.ino
+++ Rfid reader.ino
@@ -1,43 +1,98 @@
-
+#include <SPI.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);//16 zeichen 2 zeilen
 #include <MFRC522.h>
 #include <MFRC522Extended.h>
 #include <require_cpp11.h>
+#include "Adafruit_Thermal.h"
 
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);   //Hier wird festgelegt um was für einen Display es sich handelt. In diesem Fall einer mit 16 Zeichen in 2 Zeilen.
-#include <SPI.h>
-#include <MFRC522.h>
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-
 #include <SoftwareSerial.h>
-SoftwareSerial RFID(2, 3); // PIN2 125 KHZ
+//SoftwareSerial RFID(2, 3); // PIN2 125 KHZ
+#define TX_PIN 6 //   YELLOW WIRE  RX on printer
+#define RX_PIN 5 // GREEN WIRE   TX on printer
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
+Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
+
 
 int i;
+long uidtemp = 0;
+
+typedef struct
+ {
+     String name;
+     long uid;
+
+ }  artikel_type;
+
+static artikel_type artikel[30];
+
+
 
 void setup()
 {
+//RFID.begin(9600);    //125 KHZ
+Serial.begin(9600); //DEBUG
 
- RFID.begin(9600);    // start serial to RFID reader 125 KHZ
-  
-Serial.begin(9600);
+mySerial.begin(19200);
+printer.begin();
+delay(3000);
+printer.sleep();
+
 SPI.begin();
 mfrc522.PCD_Init();
 pinMode (2, OUTPUT); // led
-lcd.begin(); //Im Setup wird der LCD gestartet (anders als beim einfachen LCD Modul ohne 16,2 in den Klammern denn das wurde vorher festgelegt
+lcd.begin();
+
+
+artikel[0].name = "sonstiges";
+artikel[0].uid = 503220;
+
+artikel[1].name = "brot";
+artikel[1].uid = 2032910;
+
+artikel[2].name = "nutella";
+artikel[2].uid = 1681810;
+
+artikel[3].name = "nudeln";
+artikel[3].uid = 586010;
+
+artikel[4].name = "pesto";
+artikel[4].uid = 1027810;
+
+artikel[5].name = "parmesan";
+artikel[5].uid = 2127210;
+
+artikel[6].name = "kaese";
+artikel[6].uid = 1472110;
+
+artikel[7].name = "butter";
+artikel[7].uid = 1336410;
+
+artikel[8].name = "milch";
+artikel[8].uid = 1148510;
+
+artikel[9].name = "reis";
+artikel[9].uid = 1707710;
+
+artikel[10].name = "sirup";
+artikel[10].uid = 2012310;
+
+
 }
 
 void loop()
 {
 
-if (RFID.available() > 0) //125 KHZ
+/*if (RFID.available() > 0) //125 KHZ
   {
      i = RFID.read();
      Serial.print(i, DEC);
      Serial.print(" ");
-  }
+  }*/
   
 if ( ! mfrc522.PICC_IsNewCardPresent())
 {
@@ -56,26 +111,32 @@ for (byte i = 0; i < mfrc522.uid.size; i++)
 code=((code+mfrc522.uid.uidByte[i])*10);
 }
 
-Serial.print("Die Kartennummer lautet:");
+//Serial.print("Die Kartennummer lautet:");//DEBUG
+//Serial.println(code);
 
-Serial.println(code);
+for(i = 0; i < 30; ++i) {
+    if (artikel[i].uid == code) {
+        
+        //Serial.println(artikel[i].name);//DEBUG
+        lcd.clear();
+        lcd.setCursor(0,0);                 
+        lcd.print(artikel[i].name);
+        lcd.setCursor(0,1);
+        lcd.print("Einkaufen");
 
-
-
-if (code==2346930)
-
-{ 
-lcd.setCursor(0,0); //Ab hier kann das I2C LCD Modul genau wie das einfache LCD Modul programmiert werden.
-lcd.print("Brot");
-lcd.setCursor(0,1);  // lcd.setCursor um Zeichen und Zeile anzugeben
-lcd.print("Einkaufen"); // lcd.print um etwas auf dem Display anzeigen zu lassen.
-
-digitalWrite (2, HIGH); 
-
-delay (5000); 
-
-digitalWrite (2, LOW); 
-
-} 
+      if(artikel[i].uid == uidtemp){
+      //Serial.println("break");
+      break;
+      }
+        
+        printer.wake();
+        //Serial.println(artikel[i].name);//DEBUG
+        printer.println(artikel[i].name);
+        uidtemp = artikel[i].uid;
+        delay(1000);
+        printer.sleep();
+        
+        break;
+    }}
 
 }
